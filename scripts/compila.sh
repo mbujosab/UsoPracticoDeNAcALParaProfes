cc889d9df6101527f60e45541dc76838b2c7de93
Marcos Bujosa <mbujosab@ucm.es>
2026-02-27 17:06:42 +0100

    inclusión de texlive-plain-generic en apt.tex

--- scripts/compila.sh
+++ scripts/compila.sh
@@ -17,6 +17,14 @@ fi
 
 cd "$WORKDIR"
 
+# Permite que TeX encuentre paquetes locales "vendorizados" (AcroTeX, nacal, etc.).
+# El ':' final es importante para conservar la búsqueda por defecto de TeX.
+export TEXINPUTS=".:./acrotex:./nacal:${TEXINPUTS:-}:"
+
+# (Opcional) si tu .bib/.bst están en subcarpetas y usas \bibliography{referencias/...}
+export BIBINPUTS=".:./referencias:${BIBINPUTS:-}:"
+export BSTINPUTS=".:./referencias:${BSTINPUTS:-}:"
+
 pdflatex -interaction=batchmode "$BASENAME"
 pythontex --interpreter python:python3 "$BASENAME"
 latexmk -pdf -interaction=nonstopmode -halt-on-error "$BASENAME"
